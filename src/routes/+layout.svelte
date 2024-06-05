e78eec06ba7a379c71e07a36427cdf811b7fa6d2
Lordran <lordran@lordran.dev>
2024-06-04 23:25:13 -0700

    Init project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import '../app.css'
+</script>
+
+<ul class="flex gap-3">
+    <li><a href="/">Home</a></li>
+    <li><a href="/products">Products</a></li>
+    <li><a href="/cart">Cart</a></li>
+    <li><a href="/auth/login">Login</a></li>
+    <li><a href="/auth/register">Register</a></li>
+</ul>
+<slot/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+
+<h1>Whats up SvelteKit !!</h1>
+
