a8d9979b6cc1d8e48b91ca6b42f50fab99f51787
Lordran <lordran@lordran.dev>
2024-06-06 13:15:56 -0600

    Insert orders to db

--- src/routes/cart/+page.server.ts
+++ src/routes/cart/+page.server.ts
@@ -1,5 +1,13 @@
-import type { PageServerLoad } from "./$types";
+import { db } from '$lib/server/db/db';
+import { order } from '$lib/server/db/schema';
+import type { PageServerLoad } from './$types';
+type Order = typeof order.$inferInsert
 
-export const load : PageServerLoad = async() => {
+export const actions = {
+
+    default: async(e) => {
+        const body = await e.request.json() as Order[]
+        await db.insert(order).values(body)
+    }
 
 }
--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,15 +1,36 @@
 <script lang="ts">
-    import { getCart, removeFromCart } from '$lib/client/cart';
-    import { or } from 'drizzle-orm';
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+    import { clearCart, getCart, removeFromCart } from '$lib/client/cart';
 
     $: orders = getCart()
+
+    const handleRemoveFromCart = (i : number) =>{
+        removeFromCart(i)
+        orders = getCart()
+    }
+
+    async function handleSubmit(event: { currentTarget: EventTarget & HTMLFormElement }) {
+        console.log(event.currentTarget.action)
+		const response = await fetch(event.currentTarget.action, {
+			method: 'POST',
+			body: JSON.stringify(orders)
+		});
+        if (response.ok) {
+            clearCart()
+            orders = getCart()
+        }
+	}
+
 </script>
 
 <h1 class="font-extrabold leading-10 tracking-tight text-gray-800 text-center sm:leading-none text-5xl sm:text-5xl my-6">Cart</h1>
 
+<div class="flex justify-center mt-8">
+    <form method="POST" on:submit|preventDefault={handleSubmit}>
+        <button disabled={orders.length === 0} class="text-white bg-[#FF4F01] hover:bg-[#FF4F01] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#FF4F01] dark:hover:bg-[#ff0101e7] dark:focus:ring-blue-800">
+            Checkout
+        </button>
+    </form>
+</div>
 <div class="flex flex-wrap justify-center">
     {#each orders as order , i}
     <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700 m-5">
@@ -23,7 +44,7 @@
             </h3>
             <div class="flex items-center justify-between">
                 <span class="text-3xl font-bold  text-gray-900 dark:text-white">${order.totalPrice}</span>
-                <button on:click={()=>removeFromCart(i)} class="text-white bg-[#FF4F01] hover:bg-[#FF4F01] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#FF4F01] dark:hover:bg-[#ff0101e7] dark:focus:ring-blue-800"
+                <button on:click={()=>handleRemoveFromCart(i)} class="text-white bg-[#FF4F01] hover:bg-[#FF4F01] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#FF4F01] dark:hover:bg-[#ff0101e7] dark:focus:ring-blue-800"
                  >Remove</button>
             </div>
         </div>
